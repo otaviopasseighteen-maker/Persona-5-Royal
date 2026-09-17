2401fff3fdeb14b996e1cdd60bb96b2316685fb2
Otavio Pass <otaviopasseighteen@gmail.com>
2026-09-17 11:08:54 -0300

    Force latest router for Palace rendering

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import { renderNavigation } from './navigation.js?v=p5r-modules-20260917-5';
+import { renderNavigation } from './navigation.js?v=p5r-modules-20260917-6';
 import { renderHome } from './components.js?v=p5r-modules-20260917-5';
-import { renderPage } from './router.js?v=p5r-modules-20260917-9';
+import { renderPage } from './router.js?v=p5r-modules-20260917-10';
 
 const page = document.body.dataset.page;
 renderNavigation(document.querySelector('#site-navigation'));
