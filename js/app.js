b8269366e898a9b94dbeb86cf2276a8d565e9c01
Otavio Pass <otaviopasseighteen@gmail.com>
2026-09-17 10:38:22 -0300

    Bump router cache version for Persona Origin fix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import { renderNavigation } from './navigation.js?v=p5r-modules-20260917-5';
 import { renderHome } from './components.js?v=p5r-modules-20260917-5';
-import { renderPage } from './router.js?v=p5r-modules-20260917-8';
+import { renderPage } from './router.js?v=p5r-modules-20260917-9';
 
 const page = document.body.dataset.page;
 renderNavigation(document.querySelector('#site-navigation'));
