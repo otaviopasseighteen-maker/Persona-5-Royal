59908ada30aafe211749d90ecf7ba4f7ceadf8df
Otavio Pass <otaviopasseighteen@gmail.com>
2026-09-17 11:00:34 -0300

    Support nested Palace pages in navigation

--- js/navigation.js
+++ js/navigation.js
@@ -13,7 +13,7 @@ export function renderNavigation(target) {
   if (!target) return;
   const path = window.location.pathname;
   const inPages = path.includes('/pages/');
-  const inNestedPage = /\/pages\/(personas|characters)\//.test(path);
+  const inNestedPage = /\/pages\/(personas|characters|palaces|shadows)\//.test(path);
   const prefix = inNestedPage ? '../' : (inPages ? '' : 'pages/');
   const home = inNestedPage ? '../../index.html' : (inPages ? '../index.html' : 'index.html');
   target.innerHTML = `<a href="${home}">Home</a> ${links.map(([label, href]) => `<a href="${prefix}${href}">${label}</a>`).join('')}`;
