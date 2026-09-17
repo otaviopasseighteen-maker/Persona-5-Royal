a5db39fcc553188cfe22b0a8aa089d09625d5fc7
Otavio Pass <otaviopasseighteen@gmail.com>
2026-09-17 11:24:19 -0300

    Refresh router for Palace Shadow links

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import { renderNavigation } from './navigation.js?v=p5r-modules-20260917-6';
 import { renderHome } from './components.js?v=p5r-modules-20260917-5';
-import { renderPage } from './router.js?v=p5r-modules-20260917-10';
+import { renderPage } from './router.js?v=p5r-modules-20260917-11';
 
 const page = document.body.dataset.page;
 renderNavigation(document.querySelector('#site-navigation'));
