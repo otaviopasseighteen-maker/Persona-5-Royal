3e08a184b6e8262b6d183e396c797a1b044c33fc
Otavio Pass <otaviopasseighteen@gmail.com>
2026-09-18 09:03:56 -0300

    Activate living ambient visual layer

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 import { renderNavigation } from './navigation.js?v=p5r-modules-20260917-6';
 import { renderHome } from './components.js?v=p5r-modules-20260917-5';
-import { renderPage } from './router.js?v=p5r-characters-20260918-1';
+import { renderPage } from './router.js?v=p5r-characters-20260918-2';
 import { initMotion } from './motion.js?v=p5r-motion-20260918-1';
+import { initAmbientScene } from './ambient.js?v=p5r-ambient-20260918-1';
 
-const page = document.body.dataset.page;
+const page=document.body.dataset.page;
+initAmbientScene();
 renderNavigation(document.querySelector('#site-navigation'));
-
-if (page === 'home') {
-  renderHome(document.querySelector('#home-links'));
-} else {
-  renderPage(page, document.querySelector('#app'));
-}
-
-requestAnimationFrame(() => initMotion());
+if(page==='home') renderHome(document.querySelector('#home-links'));
+else renderPage(page,document.querySelector('#app'));
+requestAnimationFrame(()=>initMotion());
