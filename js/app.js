50f9d4b2e3f83eca94807e24f254e7fa8b0f8210
Otavio Pass <otaviopasseighteen@gmail.com>
2026-09-18 08:57:10 -0300

    Bust router cache for character redesign

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import { renderNavigation } from './navigation.js?v=p5r-modules-20260917-6';
 import { renderHome } from './components.js?v=p5r-modules-20260917-5';
-import { renderPage } from './router.js?v=p5r-modules-20260917-11';
+import { renderPage } from './router.js?v=p5r-characters-20260918-1';
 import { initMotion } from './motion.js?v=p5r-motion-20260918-1';
 
 const page = document.body.dataset.page;
